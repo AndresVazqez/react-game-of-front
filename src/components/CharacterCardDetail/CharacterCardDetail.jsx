eab790512c23da9cf22becf9e5d21b1b10e4ef2a
AndresVazqez <aavsz0811@gmail.com>
2022-02-02 21:39:30 +0100

    Characters details styles

--- src/components/CharacterCardDetail/CharacterCardDetail.jsx
+++ src/components/CharacterCardDetail/CharacterCardDetail.jsx
@@ -8,43 +8,65 @@ const CharacterCardDetail = ({ character }) => {
         <img src={character.image} alt={character.name} />
         <h4>{character.name}</h4>
       </div>
-      <div className="character__info">
-        <div className="character__info--card">
+      <div className="detail__character--info">
+
+        <div className="detail__character--info--card">
           <h2>Casa</h2>
-          {character.house}
+          <div className="detail__character--info--card--list">
+            <p>{character.house}</p>
+          </div>
+
         </div>
 
-        <div className="character__info--card">
+        <div className="detail__character--info--card">
           <h2>Alianzas</h2>
-          {character.allegiances.map((item) => (
-            <p key={item}>{item}</p>
-          ))}
+          <div className="detail__character--info--card--list">
+            {character.allegiances.map((item) => (
+              <p key={item}>{item}</p>
+            ))}
+
+          </div>
+
         </div>
 
-        <div className="character__info--card">
+        <div className="detail__character--info--card">
           <h2>Apariciones</h2>
-          {character.appearances.map((item) => (
-            <p key={item}>{item}</p>
-          ))}
+          <div className="detail__character--info--card--list">
+            {character.appearances.map((item) => (
+              <p key={item}>{item}</p>
+            ))}
+          </div>
+
         </div>
 
-        <div className="character__info--card">
+        <div className="detail__character--info--card">
           <h2>Padre</h2>
-          {character.father}
+          <div className="detail__character--info--card--list">
+            <p>{character.father}</p>
+          </div>
+
         </div>
 
-        <div className="character__info--card">
+        <div className="detail__character--info--card">
           <h2>Descendientes</h2>
-          {character.siblings.map((item) => (
-            <p key={item}>{item}</p>
-          ))}
+
+          <div className="detail__character--info--card--list">
+            {character.siblings.map((item) => (
+              <p key={item}>{item}</p>
+            ))}
+
+          </div>
+
         </div>
 
-        <div className="character__info--card">
+        <div className="detail__character--info--card">
           <h2>Títulos</h2>
-          {character.titles.map((item) => (
-            <p key={item}>{item}</p>
-          ))}
+          <div className="detail__character--info--card--list">
+            {character.titles.map((item) => (
+              <p key={item}>{item}</p>))}
+
+          </div>
+
         </div>
       </div>
     </div>
