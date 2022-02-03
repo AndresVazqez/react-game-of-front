e757efadaccf5a10ecf30f31662a9948e41c16ec
Toni M <tonims.dev@gmail.com>
2022-02-03 12:52:53 +0100

    TEST push

--- src/api/fetchToApi.js
+++ src/api/fetchToApi.js
@@ -1,38 +1,40 @@
 const CharactersURL = "https://api.got.show/api/show/characters/";
+
 const HousesURL = "https://api.got.show/api/show/houses/";
 
 export const getAllCharacters = async () => {
   const res = await fetch(CharactersURL);
-    if (res.ok) {
-        return res.json();
-    } else {
-        return [];
-    }
+  if (res.ok) {
+    return res.json();
+  } else {
+    return [];
+  }
 };
 
 export const getCharacter = async (name) => {
   const res = await fetch(`${CharactersURL}${name}`);
-    if (res.ok) {
-        return res.json();
-    } else {
-        return [];
-    }
+  if (res.ok) {
+    return res.json();
+  } else {
+    return [];
+  }
 };
 
+
 export const getAllHouses = async () => {
-    const res = await fetch(HousesURL);
-      if (res.ok) {
-          return res.json();
-      } else {
-          return [];
-      }
-  };
-  
-  export const getHouse = async (name) => {
-    const res = await fetch(`${HousesURL}${name}`);
-      if (res.ok) {
-          return res.json();
-      } else {
-          return [];
-      }
-  };
+  const res = await fetch(HousesURL);
+  if (res.ok) {
+    return res.json();
+  } else {
+    return [];
+  }
+};
+
+export const getHouse = async (name) => {
+  const res = await fetch(`${HousesURL}${name}`);
+  if (res.ok) {
+    return res.json();
+  } else {
+    return [];
+  }
+};
