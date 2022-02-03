444b77b8e8d0e2870bf713fe1222b219b2dca390
Toni M <tonims.dev@gmail.com>
2022-02-03 18:08:05 +0100

    FEAT Chronology

--- src/pages/ChronologyPage/ChronologyPage.jsx
+++ src/pages/ChronologyPage/ChronologyPage.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+
+import { useState, useEffect } from "react";
 import './ChronologyPage.scss';
+import { getAllCharacters } from "../../api/fetchToApi";
+import Chronology from "../../components/Chronology/Chronology";
+
 
 const ChronologyPage = () => {
-  return ( 
-        <div>
-            <p>Chronology</p>
+    const [characters, setCharacters] = useState([]);
+  
+    useEffect(() => {
+      getAllCharacters().then((data) => {
+        setCharacters(data);
+      });
+    }, []);
+  
+    console.log("Characters:", characters);
+  
+    return (
+      <div>
+        <h2>Cronología</h2>
+        <hr />
+        <div className="chonology-container">
+        {characters.map((character) => (
+          <Chronology key={character.id} character={character}/>
+        ))}
         </div>
-    )
-};
+      </div>
+    );
+  };
 
 export default ChronologyPage;
